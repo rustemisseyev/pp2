9e3d27a2abcb8096f9fb8061a1af059e0f15ab90
Rustem Isseyev <rustemiseev0604@gmail.com>
2025-02-06 04:55:17 +0500

    lab3

---
+++ PP2/lab3/Classes/3.py
@@ -0,0 +1,19 @@
+class Shape:
+    def __init__(self):
+        pass 
+
+    def area(self):
+        return 0  
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+rect = Rectangle(5, 10)
+print(rect.area())
